71b4af1c7caca014010d10614be68d1ca74ef0b6
Your Name <you@example.com>
2024-01-13 18:40:27 -0500

    commited

---
+++ core/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from django.contrib.admin import site
+urlpatterns = [
+    path('', include('app.urls'), name='app'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('admin/', site.urls)
+]
